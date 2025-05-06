c1df546e6c5b6dbce948777e1bae0c6fa854d0e7
Anup Das Gupta <anupdg@outlook.com>
2025-05-07 00:13:42 +0530

    Update base.js

--- base.js
+++ base.js
@@ -1,15 +1,19 @@
- var viewer = WALK.getViewer();
-  viewer.onNodeTypeClicked(function(node, position, distance) {
-    var nodeTypePath = [];
-    while (node) {
-      nodeTypePath.push(node.type);
-      node = node.parent;
-    }
-    viewer.openPopup(
-      '<div style="font-size: 1.5em">' +
-      '  Clicked nodes: ' + nodeTypePath.join(' > ') + '<br/>' +
-      '  Click distance: ' + distance.toFixed(2) + ' m' +
-      '</div>'
-    )
-    return true;
+document.addEventListener('viewerloaded', () => {
+  const viewer = WALK.getViewer();
+
+  // Wait for the scene to load enough to access nodes and materials
+  viewer.onSceneReadyToDisplay(() => {
+    // 1. Log all editable materials
+    const editableMaterials = viewer.getEditableMaterials();
+    console.log('🎨 Editable Materials:', editableMaterials);
+
+    // 2. Make all materials editable
+    editableMaterials.forEach(name => {
+      viewer.setMaterialEditable(name);
+    });
+
+    // 3. Log all mesh nodes
+    const meshNodes = viewer.findNodesOfType('Mesh');
+    console.log('🧱 Mesh Nodes:', meshNodes);
   });
+});
