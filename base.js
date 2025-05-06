82a1d9e159ade95cc7b58a7ad164cc3c7ecc0320
Anup Das Gupta <anupdg@outlook.com>
2025-05-06 21:51:03 +0530

    Update base.js

--- base.js
+++ base.js
@@ -1,1 +1,12 @@
-console.log("Test log");
+const viewer = WALK.getViewer();
+
+// Hook to run before the scene starts loading
+viewer.onSceneReadyToLoad(() => {
+  const allEditableMaterials = viewer.getEditableMaterials();
+
+  allEditableMaterials.forEach(materialName => {
+    viewer.setMaterialEditable(materialName);
+    console.log(`Material made editable: ${materialName}`);
+  });
+});
+
