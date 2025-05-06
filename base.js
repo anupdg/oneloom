09fb32035b750d51c2ab9924f228a95534dcc789
Anup Das Gupta <anupdg@outlook.com>
2025-05-07 01:13:06 +0530

    Update base.js

--- base.js
+++ base.js
@@ -1,11 +1,1 @@
 
-  const viewer = WALK.getViewer();
-var materialsNames = ['Floor_1', 'FAB_1', 'Wall_1', 'Wall_6', 'Wall_5', 'Floor_2', 'Floor_3', 'Floor_4', 'Floor_5', 'Wall_2', 'Wall_3', 'Wall_4', 'RUG_1', 'RUG_2', 'RUG_3', 'FAB_2', 'FAB_3', 'RUG_4', 'Wall_7'];
- 
-    materialsNames.forEach(name => {
-      viewer.setMaterialEditable(name);
-    });
-
-viewer.onApiUserStateChanged(function(key, value) {
-  console.log(key, value);
-});
