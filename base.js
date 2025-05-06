b6eb60226e6c6fe244f22c9865557a6f9217724a
Anup Das Gupta <anupdg@outlook.com>
2025-05-06 22:41:59 +0530

    Update base.js

--- base.js
+++ base.js
@@ -1,14 +1,10 @@
 const viewer = WALK.getViewer();
 
-// Call this IMMEDIATELY after viewer creation
-const editableMaterials = viewer.getEditableMaterials();
+// THEN attach event hooks like this
+viewer.onSceneReadyToDisplay(() => {
+  const editableMaterials = viewer.getEditableMaterials();
 editableMaterials.forEach(materialName => {
   viewer.setMaterialEditable(materialName);
   console.log(`Material made editable: ${materialName}`);
 });
-
-// THEN attach event hooks like this
-viewer.onSceneReadyToDisplay(() => {
-  console.log('Scene is ready!');
-  // You can now interact with the materials or UI
 });
