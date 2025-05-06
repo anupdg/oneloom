abb261ce641063a3c108ca259f8a98df7f5fe55d
Anup Das Gupta <anupdg@outlook.com>
2025-05-07 00:35:17 +0530

    Update base.js

--- base.js
+++ base.js
@@ -1,19 +1,9 @@
-document.addEventListener('viewerloaded', () => {
+
   const viewer = WALK.getViewer();
-
-  // Wait for the scene to load enough to access nodes and materials
-  viewer.onSceneReadyToDisplay(() => {
-    // 1. Log all editable materials
-    const editableMaterials = viewer.getEditableMaterials();
-    console.log('🎨 Editable Materials:', editableMaterials);
-
-    // 2. Make all materials editable
-    editableMaterials.forEach(name => {
+var materialsNames = ['Floor_1', 'FAB_1', 'Wall_1', 'Wall_6', 'Wall_5', 'Floor_2', 'Floor_3', 'Floor_4', 'Floor_5', 'Wall_2', 'Wall_3', 'Wall_4', 'RUG_1', 'RUG_2', 'RUG_3', 'FAB_2', 'FAB_3', 'RUG_4', 'Wall_7'];
+ 
+    materialsNames.forEach(name => {
       viewer.setMaterialEditable(name);
     });
 
-    // 3. Log all mesh nodes
-    const meshNodes = viewer.findNodesOfType('Mesh');
-    console.log('🧱 Mesh Nodes:', meshNodes);
-  });
-});
+
