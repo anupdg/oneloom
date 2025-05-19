18387c9b9c16581c28de527eeb3d7931fce0fb93
Derrick PS <83653388+derrickps@users.noreply.github.com>
2025-05-19 14:51:20 +0530

    Update base.js

--- base.js
+++ base.js
@@ -12,7 +12,7 @@ const textureCache = {};
 
 // Hardcoded material name - change this when needed
 // const MATERIAL_NAME = "Floor";
-const MATERIAL_NAME = "walls";
+const MATERIAL_NAME = "Wall_1";
 
 // 'Base','Wall plaster','Floor','Ceiling','Table'
 
