f75c910a17d5f7f3c339d246c47482fabec017a0
Anup Das Gupta <anupdg@outlook.com>
2025-06-27 20:15:16 +0530

    Update fireplace_lounge_01.js

--- fireplace_lounge_01.js
+++ fireplace_lounge_01.js
@@ -7,18 +7,27 @@ function initViewer() {
 }
 
 window.addEventListener("message", function (e) {
-  if(e.data && "MATERIALS_EDITABLE" === e.data.type){
+  if(e.data && 'FC9B8633-FB7E-4CDB-B9B4-9C7402805EB8' === e.data.type){
     console.log("MATERIALS_EDITABLE", e.data)
     e.data.extensions.forEach(materialName => {
       window.viewer.setMaterialEditable(materialName);
     });
-  }else if(e.data && "NODES_EDITABLE" === e.data.type){
+  }else if(e.data && 'B3331D7E-5FEA-4763-959F-BB468F7A2252' === e.data.type){
     console.log("NODES_EDITABLE", e.data)
     e.data.nodes.forEach(node => {
       window.viewer.setNodeTypeEditable(node);
     });
-  } else if(e.data && "GOTO_VIEW" === e.data.type){
+  } else if(e.data && '0047F251-C4C9-4163-BD66-E78E2096AB0B' === e.data.type){
     this.window.viewer.switchToView(e.data.view);
+  }else if(e.data && '980A9415-2888-4596-BDB0-37DE9CA99702' === e.data.type){
+    for(const nodeName in e.data.nodesTohide){
+      for (const node of viewer.findNodesOfType(nodeName)) {
+        node.hide();  
+      }
+    }
+    for (const node of viewer.findNodesOfType(e.data.node)) {
+        node.hide();  
+      }
   }
 })
 
