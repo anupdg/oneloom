de3db2c17a513adc4c30a0a81c84d022059897ad
nath-neeraj-dgtc <neeraj.nath@dgtc.in>
2025-07-07 20:35:27 +0530

    Update bvp_fireplace_lounge_01.js

--- bvp_fireplace_lounge_01.js
+++ bvp_fireplace_lounge_01.js
@@ -1,60 +1,10 @@
 function initViewer() {
   window.viewer = WALK.getViewer();
 
-  // Save the built-in Shapespark handler
-  const originalHandler = window.viewer.onSceneReadyToDisplay;
-
-  // Create a new combined handler
-  window.viewer.onSceneReadyToDisplay = () => {
-    // Call Shapespark's default initialization (restores menu buttons)
-    if (typeof originalHandler === 'function') {
-      originalHandler();
-    }
-
-    // Your custom logic
-    window.viewer.onNodeTypeClicked(function(node){
-      console.log("node", node);
-    });
-
-    window.parent.postMessage(
-      { type: '56C8AB6F-5F86-441A-9E7B-84CF4A81CDC9', payload: {} },
-      "*"
-    );
-  };
+  window.viewer.whenSceneReady(() => {
+    console.log('Scene is ready.');
+  });
 }
-
-window.addEventListener("message", function (e) {
-  if(e.data && 'FC9B8633-FB7E-4CDB-B9B4-9C7402805EB8' === e.data.type){
-    console.log("MATERIALS_EDITABLE", e.data);
-    e.data.extensions.forEach(materialName => {
-      window.viewer.setMaterialEditable(materialName);
-    });
-  } else if(e.data && 'B3331D7E-5FEA-4763-959F-BB468F7A2252' === e.data.type){
-    console.log("NODES_EDITABLE", e.data);
-    e.data.nodes.forEach(node => {
-      window.viewer.setNodeTypeEditable(node);
-    });
-  } else if(e.data && '0047F251-C4C9-4163-BD66-E78E2096AB0B' === e.data.type){
-    this.window.viewer.switchToView(e.data.view);
-  } else if(e.data && '980A9415-2888-4596-BDB0-37DE9CA99702' === e.data.type){
-    for (const nodeName of e.data.nodesTohide) {
-      for (const node of window.viewer.findNodesOfType(nodeName)) {
-        node.hide();  
-      }
-    }
-    for (const node of window.viewer.findNodesOfType(e.data.node)) {
-      node.show();  
-    }
-    window.viewer.requestFrame();
-  } else if(e.data && '22D78DEB-39B2-4DB4-A560-5B0C143B02F8' === e.data.type){
-    const material = window.viewer.findMaterial(e.data.material);
-    for (const node of window.viewer.findNodesOfType(e.data.node)) {
-      window.viewer.setMaterialForMesh(material, node.mesh);
-    }
-    window.viewer.requestFrame();
-  }
-});
-
 document.addEventListener("DOMContentLoaded", function () {
   initViewer();
 });
