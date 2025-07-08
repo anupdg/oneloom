c82468120349e8d7c20d89f1d1f44c8cb9180757
nath-neeraj-dgtc <neeraj.nath@dgtc.in>
2025-07-08 20:29:47 +0530

    Update bvp_fireplace_lounge_01.js

--- bvp_fireplace_lounge_01.js
+++ bvp_fireplace_lounge_01.js
@@ -46,6 +46,7 @@ window.addEventListener("message", function(e) {
     e.data.nodes.forEach(node => {
       window.viewer.setNodeTypeEditable(node);
     });
+    window.viewer.setMaterialEditable([]);
   } else if (e.data && '0047F251-C4C9-4163-BD66-E78E2096AB0B' === e.data.type) {
     window.viewer.switchToView(e.data.view);
   } else if (e.data && '980A9415-2888-4596-BDB0-37DE9CA99702' === e.data.type) {
