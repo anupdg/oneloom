7fa28aa14405c35800b582de187ddfb18acfad2f
nath-neeraj-dgtc <neeraj.nath@dgtc.in>
2025-07-09 19:59:38 +0530

    Update bvp_fireplace_lounge_01.js

--- bvp_fireplace_lounge_01.js
+++ bvp_fireplace_lounge_01.js
@@ -5,7 +5,79 @@ function initViewer() {
 
     viewer.setMaterialEditable([]);
     
-    let sofaSelectionApplied = false;
+    // Create a container div to hold labels on top of the canvas
+    const labelsContainer = document.createElement('div');
+    labelsContainer.id = 'labels-container';
+    labelsContainer.style.position = 'absolute';
+    labelsContainer.style.top = '0';
+    labelsContainer.style.left = '0';
+    labelsContainer.style.pointerEvents = 'none'; // Allow clicks to pass through by default
+    labelsContainer.style.width = '100%';
+    labelsContainer.style.height = '100%';
+    document.body.appendChild(labelsContainer);
+
+    // Define hotspots with pre-measured positions from scene.json
+    const hotspots = [
+      {
+        id: 'Floor_Living_003',
+        position: [2.0, 0.05, -3.5],
+        icon: 'https://cdn-icons-png.flaticon.com/512/1828/1828884.png'
+      },
+      {
+        id: 'sofa',
+        position: [1.0, 0.6, -2.0],
+        icon: 'https://cdn-icons-png.flaticon.com/512/1828/1828884.png'
+      },
+      {
+        id: 'Liv_wall_001',
+        position: [1.5, 1.5, -4.0],
+        icon: 'https://cdn-icons-png.flaticon.com/512/1828/1828884.png'
+      }
+      // Add more as needed
+    ];
+
+    // Create HTML elements for each hotspot
+    hotspots.forEach(hs => {
+      const div = document.createElement('div');
+      div.className = 'custom-label';
+      div.style.position = 'absolute';
+      div.style.width = '32px';
+      div.style.height = '32px';
+      div.style.pointerEvents = 'auto'; // So this element *can* be clicked
+      div.style.cursor = 'pointer';
+      div.innerHTML = `<img src="${hs.icon}" style="width:32px;height:32px;">`;
+      div.onclick = () => {
+        console.log(`Custom picker clicked: ${hs.id}`);
+        window.parent.postMessage(
+          {
+            type: "SELECT_FROM_SCENE_CLICK",
+            payload: { nodeType: hs.id }
+          },
+          "*"
+        );
+      };
+      hs.element = div;
+      labelsContainer.appendChild(div);
+    });
+
+    // Animate: reposition hotspots each frame
+    viewer.onFrameViewer(() => {
+      const camera = viewer.getCamera();
+      hotspots.forEach(hs => {
+        // Convert 3D to 2D screen coordinates
+        const screenPos = camera.worldToScreen(hs.position);
+
+        if (screenPos) {
+          hs.element.style.display = "block";
+          hs.element.style.left = `${screenPos[0] - 16}px`; // Center icon
+          hs.element.style.top = `${screenPos[1] - 16}px`;
+        } else {
+          // Behind the camera
+          hs.element.style.display = "none";
+        }
+      });
+    });
+
 
     viewer.onNodeTypeClicked(function(node) {
       console.log("node", node);
