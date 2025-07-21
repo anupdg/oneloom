6ea065a928aa1039ed592c0acdec8f143895e5af
nath-neeraj-dgtc <neeraj.nath@dgtc.in>
2025-07-21 13:18:58 +0530

    Update bvp_fireplace_lounge_01.js

--- bvp_fireplace_lounge_01.js
+++ bvp_fireplace_lounge_01.js
@@ -212,8 +212,6 @@ window.addEventListener("message", function (e) {
   }else if(e.data && '22D78DEB-39B2-4DB4-A560-5B0C143B02F8' === e.data.type){
     const material = window.viewer.findMaterial(e.data.material);
     for (const node of window.viewer.findNodesOfType(e.data.node)) {
-      window.viewer.setNodeTypeEditable(node);
-      window.viewer.setMaterialEditable(material);
       window.viewer.setMaterialForMesh(material, node.mesh)
     }
     window.viewer.requestFrame();
